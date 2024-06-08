43a99f9c8f6be2ca378cbe30207493ee32e35165
Polaris Starnor <pola@localhost.localdomain>
2024-06-08 13:57:07 -0600

    Initial Commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const QUINTESSENCE_SYS = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+QUINTESSENCE_SYS.abilities = {
+  str: 'QUINTESSENCE_SYS.Ability.Str.long',
+  dex: 'QUINTESSENCE_SYS.Ability.Dex.long',
+  con: 'QUINTESSENCE_SYS.Ability.Con.long',
+  int: 'QUINTESSENCE_SYS.Ability.Int.long',
+  wis: 'QUINTESSENCE_SYS.Ability.Wis.long',
+  cha: 'QUINTESSENCE_SYS.Ability.Cha.long',
+};
+
+QUINTESSENCE_SYS.abilityAbbreviations = {
+  str: 'QUINTESSENCE_SYS.Ability.Str.abbr',
+  dex: 'QUINTESSENCE_SYS.Ability.Dex.abbr',
+  con: 'QUINTESSENCE_SYS.Ability.Con.abbr',
+  int: 'QUINTESSENCE_SYS.Ability.Int.abbr',
+  wis: 'QUINTESSENCE_SYS.Ability.Wis.abbr',
+  cha: 'QUINTESSENCE_SYS.Ability.Cha.abbr',
+};
