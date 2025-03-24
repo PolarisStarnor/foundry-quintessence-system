3192e5bb52835ed4210788a7485155296819ef63
Polaris Starnor <polarisstarnor@gmail.com>
2025-03-24 13:38:17 -0600

    Adding more function for hit/roll

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -4,12 +4,18 @@
  */
 export class QuintessenceSystemActor extends Actor {
 
-	passTurn() {
-		// Update actor's statuses when turn passes
-		const systemData = actorData.system;
-		const status = systemData.status;
+    passTurn() {
+        // Update actor's statuses when turn passes
+        const systemData = actorData.system;
+        const health = systemData.health;
+        const status = systemData.status;
+
+        // DoT
+        health.value -= status.burn;
 
         // Stuff that absolutely does not stack at the end of a turn
+        status.attack_mod = 0;
+        status.fragility = 0;
         status.coin_count_mod = 0;
         status.coin_power_mod = 0;
         status.base_power_mod = 0;
@@ -18,76 +24,101 @@ export class QuintessenceSystemActor extends Actor {
         status.bind = 0;
 
         // Stuff Meant to stack until the end of a turn
-        status.burn *= 0.5;
-        status.bleed *= 0.5;
-        status.sinking *= 0.5;
-        status.rupture *= 0.5;
-        status.tremor *= 0.5;
+        // Actually like half of these are wrong
+        status.burn = math.round(status.burn * 0.5);
+        status.bleed = math.round(status.bleed * 0.5);
+        status.sinking = math.round(status.sinking * 0.5);
+        status.rupture = math.round(status.rupture * 0.5);
+        status.tremor = math.round(status.tremor * 0.5);
 
-	}
-	
-	/** @override */
-	prepareData() {
-		// Prepare data for the actor. Calling the super version of this executes
-		// the following, in order: data reset (to clear active effects),
-		// prepareBaseData(), prepareEmbeddedDocuments() (including active effects),
-		// prepareDerivedData().
-		super.prepareData();
-	}
+    }
 
-	/** @override */
-	prepareBaseData() {
-		// Data modifications in this step occur before processing embedded
-		// documents or derived data.
-	}
+    onHit() {
+        const systemData = actorData.system;
+        const health = systemData.health;
+        const sanity = systemData.sanity;
+        const status = systemData.status;
 
-	/**
-	 * @override
-	 * Augment the actor source data with additional dynamic data. Typically,
-	 * you'll want to handle most of your calculated/derived data in this step.
-	 * Data calculated in this step should generally not exist in template.json
-	 * (such as ability modifiers rather than ability scores) and should be
-	 * available both inside and outside of character sheets (such as if an actor
-	 * is queried and has a roll executed directly from it).
-	 */
-	prepareDerivedData() {
-		const actorData = this;
-		const systemData = actorData.system;
-		const flags = actorData.flags.foundryquintessencesystem || {};
+        health.value -= status.rupture;
+        sanity.value -= status.sinking;
 
-		// Make separate methods for each Actor type (character, npc, etc.) to keep
-		// things organized.
-		this._prepareCharacterData(actorData);
-	}
+        status.sinking = math.round(status.sinking * 0.5);
+        status.rupture = math.round(status.rupture * 0.5);
+    }
 
-	/**
-	 * Prepare Character type specific data
-	 */
-	_prepareCharacterData(actorData) {
-		if (actorData.type !== 'character') return;
+    onRoll() {
+        const systemData = actorData.system
+        const health = systemData.health;
+        const status = systemData.status;
 
-		// Make modifications to data here. For example:
-		const systemData = actorData.system;
-		const status = systemData.status;
+        health.value -= status.bleed
 
-		// // Loop through ability scores, and add their modifiers to our sheet output.
-		// for (let [key, ability] of Object.entries(systemData.abilities)) {
-		//	 // Calculate the modifier using d20 rules.
-		//	 // ability.mod = Math.floor((ability.value - 10) / 2);
-		// }
-	}
-	/**
-	 * Override getRollData() that's supplied to rolls.
-	 */
-	getRollData() {
-		// Starts off by populating the roll data with a shallow copy of `this.system`
-		const data = { ...this.system };
+        status.bleed = math.round(status.bleed * 0.5);
 
-		// Prepare character roll data.
-		this._getCharacterRollData(data);
+    }
 
-		return data;
-	}
+    /** @override */
+    prepareData() {
+        // Prepare data for the actor. Calling the super version of this executes
+        // the following, in order: data reset (to clear active effects),
+        // prepareBaseData(), prepareEmbeddedDocuments() (including active effects),
+        // prepareDerivedData().
+        super.prepareData();
+    }
+
+    /** @override */
+    prepareBaseData() {
+        // Data modifications in this step occur before processing embedded
+        // documents or derived data.
+    }
+
+    /**
+     * @override
+     * Augment the actor source data with additional dynamic data. Typically,
+     * you'll want to handle most of your calculated/derived data in this step.
+     * Data calculated in this step should generally not exist in template.json
+     * (such as ability modifiers rather than ability scores) and should be
+     * available both inside and outside of character sheets (such as if an actor
+     * is queried and has a roll executed directly from it).
+     */
+    prepareDerivedData() {
+        const actorData = this;
+        const systemData = actorData.system;
+        const flags = actorData.flags.foundryquintessencesystem || {};
+
+        // Make separate methods for each Actor type (character, npc, etc.) to keep
+        // things organized.
+        this._prepareCharacterData(actorData);
+    }
+
+    /**
+     * Prepare Character type specific data
+     */
+    _prepareCharacterData(actorData) {
+        if (actorData.type !== 'character') return;
+
+        // Make modifications to data here. For example:
+        const systemData = actorData.system;
+        const status = systemData.status;
+
+        // // Loop through ability scores, and add their modifiers to our sheet output.
+        // for (let [key, ability] of Object.entries(systemData.abilities)) {
+        //   // Calculate the modifier using d20 rules.
+        //   // ability.mod = Math.floor((ability.value - 10) / 2);
+        // }
+    }
+    /**
+     * Override getRollData() that's supplied to rolls.
+     */
+    getRollData() {
+        // Starts off by populating the roll data with a shallow copy of `this.system`
+        const data = { ...this.system };
+
+        // Prepare character roll data.
+        this._getCharacterRollData(data);
+
+        return data;
+    }
 
     getSkills() {
         const skills = []
@@ -97,23 +128,29 @@ export class QuintessenceSystemActor extends Actor {
         return skills;
     }
 
-	/**
-	 * Prepare character roll data.
-	 */
-	_getCharacterRollData(data) {
-		if (this.type !== 'character') return;
+    /**
+     * Prepare character roll data.
+     */
+    _getCharacterRollData(data) {
+        if (this.type !== 'character') return;
 
-		// Copy the ability scores to the top level, so that rolls can use
-		// formulas like `@str.mod + 4`.
-		if (data.abilities) {
-			for (let [k, v] of Object.entries(data.abilities)) {
-				data[k] = foundry.utils.deepClone(v);
-			}
-		}
+        // Copy the ability scores to the top level, so that rolls can use
+        // formulas like `@str.mod + 4`.
+        if (data.abilities) {
+            for (let [k, v] of Object.entries(data.abilities)) {
+                data[k] = foundry.utils.deepClone(v);
+            }
+        }
 
-		// // Add level for easier access, or fall back to 0.
-		// if (data.attributes.level) {
-		//	 data.lvl = data.attributes.level.value ?? 0;
-		// }
-	}
+        // // Add level for easier access, or fall back to 0.
+        // if (data.attributes.level) {
+        //   data.lvl = data.attributes.level.value ?? 0;
+        // }
+    }
+
+    rollInitiative(options?) {
+        let ret = super(options?)
+        passTurn()
+        return ret;
+    }
 }
