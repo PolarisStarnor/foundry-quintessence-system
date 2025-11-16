49c3304b3d51289d534c93c215665df997e01b22
PolarisStarnor <polarisstarnor@gmail.com>
2025-11-15 23:18:11 -0700

    space-based-tabs

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -25,19 +25,19 @@ export class QuintessenceSystemActorSheet extends HandlebarsApplicationMixin(Act
     /**
      * Non-method configurations
      */
-	static DEFAULT_OPTIONS = {
+    static DEFAULT_OPTIONS = {
         tag: 'form',
-		classes: ['foundry-quintessence-system', 'sheet', 'actor'],
+        classes: ['foundry-quintessence-system', 'sheet', 'actor'],
         position: {
             width: 600,
             height: 600,
         },
-		tabs: [
-			{
-				navSelector: '.sheet-tabs',
-				contentSelector: '.sheet-body',
-				initial: 'features',
-			},
+        tabs: [
+            {
+                navSelector: '.sheet-tabs',
+                contentSelector: '.sheet-body',
+                initial: 'features',
+            },
         ],
     }
 
@@ -71,229 +71,229 @@ export class QuintessenceSystemActorSheet extends HandlebarsApplicationMixin(Act
         },
     }
 
-	/* -------------------------------------------- */
+    /* -------------------------------------------- */
 
-	/** @override */
-	async _prepareContext(options) {
-		// Retrieve the data structure from the base sheet. You can inspect or log
-		// the context variable to see the structure, but some key properties for
-		// sheets are the actor object, the data object, whether or not it's
-		// editable, the items array, and the effects array.
-		const context = super._prepareContext(options);
+    /** @override */
+    async _prepareContext(options) {
+        // Retrieve the data structure from the base sheet. You can inspect or log
+        // the context variable to see the structure, but some key properties for
+        // sheets are the actor object, the data object, whether or not it's
+        // editable, the items array, and the effects array.
+        const context = super._prepareContext(options);
 
-		// Use a safe clone of the actor data for further operations.
-		const actorData = this.document.toObject(false);
+        // Use a safe clone of the actor data for further operations.
+        const actorData = this.document.toObject(false);
 
-		// Add the actor's data to context.data for easier access, as well as flags.
-		context.system = actorData.system;
-		context.flags = actorData.flags;
+        // Add the actor's data to context.data for easier access, as well as flags.
+        context.system = actorData.system;
+        context.flags = actorData.flags;
 
-		// Adding a pointer to CONFIG.QUINTESSENCE_SYS
-		context.config = CONFIG.QUINTESSENCE_SYS;
+        // Adding a pointer to CONFIG.QUINTESSENCE_SYS
+        context.config = CONFIG.QUINTESSENCE_SYS;
 
-		// Prepare character data and items.
-		if (actorData.type == 'character') {
-			this._prepareItems(context);
-			this._prepareCharacterData(context);
-		}
+        // Prepare character data and items.
+        if (actorData.type == 'character') {
+            this._prepareItems(context);
+            this._prepareCharacterData(context);
+        }
 
-		// Enrich biography info for display
-		// Enrichment turns text like `[[/r 1d20]]` into buttons
-		context.enrichedBiography = await TextEditor.enrichHTML(
-			this.actor.system.biography,
-			{
-				// Whether to show secret blocks in the finished html
-				secrets: this.document.isOwner,
-				// Necessary in v11, can be removed in v12
-				async: true,
-				// Data to fill in for inline rolls
-				rollData: this.actor.getRollData(),
-				// Relative UUID resolution
-				relativeTo: this.actor,
-			}
-		);
+        // Enrich biography info for display
+        // Enrichment turns text like `[[/r 1d20]]` into buttons
+        context.enrichedBiography = await TextEditor.enrichHTML(
+            this.actor.system.biography,
+            {
+                // Whether to show secret blocks in the finished html
+                secrets: this.document.isOwner,
+                // Necessary in v11, can be removed in v12
+                async: true,
+                // Data to fill in for inline rolls
+                rollData: this.actor.getRollData(),
+                // Relative UUID resolution
+                relativeTo: this.actor,
+            }
+        );
 
-		// Prepare active effects
-		context.effects = prepareActiveEffectCategories(
-			// A generator that returns all effects stored on the actor
-			// as well as any items
-			this.actor.allApplicableEffects()
-		);
+        // Prepare active effects
+        context.effects = prepareActiveEffectCategories(
+            // A generator that returns all effects stored on the actor
+            // as well as any items
+            this.actor.allApplicableEffects()
+        );
 
-		return context;
-	}
-
-	/**
-	 * Character-specific context modifications
-	 *
-	 * @param {object} context The context object to mutate
-	 */
-	_prepareCharacterData(context) {
-		// This is where you can enrich character-specific editor fields
-		// or setup anything else that's specific to this type
-	}
-
-	/**
-	 * Organize and classify Items for Actor sheets.
-	 *
-	 * @param {object} context The context object to mutate
-	 */
-	_prepareItems(context) {
-		// Initialize containers.
-		const gear = [];
-		const passives = [];
-		const skills = [];
-
-		// // Iterate through items, allocating to containers
-		// for (let i of context.items) {
-		// 	i.img = i.img || Item.DEFAULT_ICON;
-		// 	// Append to skills.
-		// 	if (i.type === 'skill') {
-		// 		skills.push(i);
-		// 	}
-		// 	// Append to abilities.
-		// 	else if (i.type === 'passive') {
-		// 		passives.push(i);
-		// 	}
-		// 	// Append to items.
-		// 	else if (i.type === 'item') {
-        //         gear.push(i);
-		// 	}
-		// }
-
-		// // Assign and return
-		// context.gear = gear;
-		// context.passives = passives;
-		// context.skills = skills;
-	}
-
-	/* -------------------------------------------- */
-
-	/** @override */
-	activateListeners(html) {
-		super.activateListeners(html);
-
-		// Render the item sheet for viewing/editing prior to the editable check.
-		html.on('click', '.item-edit', (ev) => {
-			const li = $(ev.currentTarget).parents('.item');
-			const item = this.actor.items.get(li.data('itemId'));
-			item.sheet.render(true);
-		});
-
-		// -------------------------------------------------------------
-		// Everything below here is only needed if the sheet is editable
-		if (!this.isEditable) return;
-
-		// Add Inventory Item
-		html.on('click', '.item-create', this._onItemCreate.bind(this));
-
-		// Delete Inventory Item
-		html.on('click', '.item-delete', (ev) => {
-			const li = $(ev.currentTarget).parents('.item');
-			const item = this.actor.items.get(li.data('itemId'));
-			item.delete();
-			li.slideUp(200, () => this.render(false));
-		});
-
-		// Active Effect management
-		html.on('click', '.effect-control', (ev) => {
-			const row = ev.currentTarget.closest('li');
-			const document =
-				  row.dataset.parentId === this.actor.id
-				  ? this.actor
-				  : this.actor.items.get(row.dataset.parentId);
-			onManageActiveEffect(ev, document);
-		});
-
-		// Rollable abilities.
-		html.on('click', '.rollable', this._onRoll.bind(this));
-
-		// Drag events for macros.
-		if (this.actor.isOwner) {
-			let handler = (ev) => this._onDragStart(ev);
-			html.find('li.item').each((i, li) => {
-				if (li.classList.contains('inventory-header')) return;
-				li.setAttribute('draggable', true);
-				li.addEventListener('dragstart', handler, false);
-			});
-		}
-
-        // Clash Dialog
-		html.on('click', '.clashable', this._clashDialog.bind(this));
-	}
+        return context;
+    }
 
     /**
-	 * Create a dialog for clashing with other characters
-	 *
-	 */
+     * Character-specific context modifications
+     *
+     * @param {object} context The context object to mutate
+     */
+    _prepareCharacterData(context) {
+        // This is where you can enrich character-specific editor fields
+        // or setup anything else that's specific to this type
+    }
+
+    /**
+     * Organize and classify Items for Actor sheets.
+     *
+     * @param {object} context The context object to mutate
+     */
+    _prepareItems(context) {
+        // Initialize containers.
+        const gear = [];
+        const passives = [];
+        const skills = [];
+
+        // // Iterate through items, allocating to containers
+        // for (let i of context.items) {
+        //      i.img = i.img || Item.DEFAULT_ICON;
+        //      // Append to skills.
+        //      if (i.type === 'skill') {
+        //          skills.push(i);
+        //      }
+        //      // Append to abilities.
+        //      else if (i.type === 'passive') {
+        //          passives.push(i);
+        //      }
+        //      // Append to items.
+        //      else if (i.type === 'item') {
+        //         gear.push(i);
+        //      }
+        // }
+
+        // // Assign and return
+        // context.gear = gear;
+        // context.passives = passives;
+        // context.skills = skills;
+    }
+
+    /* -------------------------------------------- */
+
+    /** @override */
+    activateListeners(html) {
+        super.activateListeners(html);
+
+        // Render the item sheet for viewing/editing prior to the editable check.
+        html.on('click', '.item-edit', (ev) => {
+            const li = $(ev.currentTarget).parents('.item');
+            const item = this.actor.items.get(li.data('itemId'));
+            item.sheet.render(true);
+        });
+
+        // -------------------------------------------------------------
+        // Everything below here is only needed if the sheet is editable
+        if (!this.isEditable) return;
+
+        // Add Inventory Item
+        html.on('click', '.item-create', this._onItemCreate.bind(this));
+
+        // Delete Inventory Item
+        html.on('click', '.item-delete', (ev) => {
+            const li = $(ev.currentTarget).parents('.item');
+            const item = this.actor.items.get(li.data('itemId'));
+            item.delete();
+            li.slideUp(200, () => this.render(false));
+        });
+
+        // Active Effect management
+        html.on('click', '.effect-control', (ev) => {
+            const row = ev.currentTarget.closest('li');
+            const document =
+                  row.dataset.parentId === this.actor.id
+                  ? this.actor
+                  : this.actor.items.get(row.dataset.parentId);
+            onManageActiveEffect(ev, document);
+        });
+
+        // Rollable abilities.
+        html.on('click', '.rollable', this._onRoll.bind(this));
+
+        // Drag events for macros.
+        if (this.actor.isOwner) {
+            let handler = (ev) => this._onDragStart(ev);
+            html.find('li.item').each((i, li) => {
+                if (li.classList.contains('inventory-header')) return;
+                li.setAttribute('draggable', true);
+                li.addEventListener('dragstart', handler, false);
+            });
+        }
+
+        // Clash Dialog
+        html.on('click', '.clashable', this._clashDialog.bind(this));
+    }
+
+    /**
+     * Create a dialog for clashing with other characters
+     *
+     */
     async _clashDialog(event) {
-		event.preventDefault();
+        event.preventDefault();
         const clash = new Clash(this.actor)
-		clash.setTarget(getViewedActors()[0]); // Low effort default target
+        clash.setTarget(getViewedActors()[0]); // Low effort default target
         const app = new ClashApplication(clash);
         app.render(true);
     }
 
-	/**
-	 * Handle creating a new Owned Item for the actor using initial data defined in the HTML dataset
-	 * @param {Event} event   The originating click event
-	 * @private
-	 */
-	async _onItemCreate(event) {
-		event.preventDefault();
-		const header = event.currentTarget;
-		// Get the type of item to create.
-		const type = header.dataset.type;
-		// Grab any data associated with this control.
-		const data = duplicate(header.dataset);
-		// Initialize a default name.
-		const name = `New ${type.capitalize()}`;
-		// Prepare the item object.
-		const itemData = {
-			name: name,
-			type: type,
-			system: data,
-		};
-		// Remove the type from the dataset since it's in the itemData.type prop.
-		delete itemData.system['type'];
+    /**
+     * Handle creating a new Owned Item for the actor using initial data defined in the HTML dataset
+     * @param {Event} event   The originating click event
+     * @private
+     */
+    async _onItemCreate(event) {
+        event.preventDefault();
+        const header = event.currentTarget;
+        // Get the type of item to create.
+        const type = header.dataset.type;
+        // Grab any data associated with this control.
+        const data = duplicate(header.dataset);
+        // Initialize a default name.
+        const name = `New ${type.capitalize()}`;
+        // Prepare the item object.
+        const itemData = {
+            name: name,
+            type: type,
+            system: data,
+        };
+        // Remove the type from the dataset since it's in the itemData.type prop.
+        delete itemData.system['type'];
 
-		// Finally, create the item!
-		return await Item.create(itemData, { parent: this.actor });
-	}
+        // Finally, create the item!
+        return await Item.create(itemData, { parent: this.actor });
+    }
 
-	/**
-	 * Handle clickable rolls.
-	 * @param {Event} event   The originating click event
-	 * @private
-	 */
-	_onRoll(event) {
-		event.preventDefault();
-		const element = event.currentTarget;
-		const dataset = element.dataset;
+    /**
+     * Handle clickable rolls.
+     * @param {Event} event   The originating click event
+     * @private
+     */
+    _onRoll(event) {
+        event.preventDefault();
+        const element = event.currentTarget;
+        const dataset = element.dataset;
 
-		// Handle item rolls.
-		if (dataset.rollType) {
-			const itemId = element.closest('.item').dataset.itemId;
-			const item = this.actor.items.get(itemId);
-			if (dataset.rollType == 'skill-check') {
-				if (item) return item.roll("check");
-			}else if (dataset.rollType == 'skill-damage') {
+        // Handle item rolls.
+        if (dataset.rollType) {
+            const itemId = element.closest('.item').dataset.itemId;
+            const item = this.actor.items.get(itemId);
+            if (dataset.rollType == 'skill-check') {
+                if (item) return item.roll("check");
+            }else if (dataset.rollType == 'skill-damage') {
                 if (item) return item.roll("damage");
             }
-		}
+        }
 
 
-		// Handle rolls that supply the formula directly.
-		if (dataset.roll) {
-			let label = dataset.label ? `[ability] ${dataset.label}` : '';
-			let roll = new Roll(dataset.roll, this.actor.getRollData());
-			roll.toMessage({
-				speaker: ChatMessage.getSpeaker({ actor: this.actor }),
-				flavor: label,
-				rollMode: game.settings.get('core', 'rollMode'),
-			});
-			return roll;
-		}
-	}
+        // Handle rolls that supply the formula directly.
+        if (dataset.roll) {
+            let label = dataset.label ? `[ability] ${dataset.label}` : '';
+            let roll = new Roll(dataset.roll, this.actor.getRollData());
+            roll.toMessage({
+                speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+                flavor: label,
+                rollMode: game.settings.get('core', 'rollMode'),
+            });
+            return roll;
+        }
+    }
 
 }
