7630c4bdcbf77e56bc914b39afb53edfef1d80e6
PolarisStarnor <polarisstarnor@gmail.com>
2025-12-09 00:19:19 -0700

    Pretty sure onRender is deprecated

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -131,11 +131,15 @@ export class QuintessenceSystemActorSheet extends HandlebarsApplicationMixin(Act
             this.actor.allApplicableEffects()
         );
 
+        console.log(context);
         return context;
     }
 
     static async formHandler(event, form, formData) {
         // TODO
+        console.log(event);
+        console.log(form);
+        console.log(formData);
         return
     }
 
@@ -186,58 +190,58 @@ export class QuintessenceSystemActorSheet extends HandlebarsApplicationMixin(Act
     /* -------------------------------------------- */
 
     /** @override */
-    _onRender(context, options) {
-        super._onRender(context, options);
-        const html = $(this.element); // Thing to keep JQuery or smth
+    // _onRender(context, options) {
+    //     super._onRender(context, options);
+    //     const html = $(this.element); // Thing to keep JQuery or smth
 
-        // Render the item sheet for viewing/editing prior to the editable check.
-        html.on('click', '.item-edit', (ev) => {
-            const li = $(ev.currentTarget).parents('.item');
-            const item = this.actor.items.get(li.data('itemId'));
-            item.sheet.render(true);
-        });
+    //     // Render the item sheet for viewing/editing prior to the editable check.
+    //     html.on('click', '.item-edit', (ev) => {
+    //         const li = $(ev.currentTarget).parents('.item');
+    //         const item = this.actor.items.get(li.data('itemId'));
+    //         item.sheet.render(true);
+    //     });
 
-        // -------------------------------------------------------------
-        // Everything below here is only needed if the sheet is editable
-        if (!this.isEditable) return;
+    //     // -------------------------------------------------------------
+    //     // Everything below here is only needed if the sheet is editable
+    //     if (!this.isEditable) return;
 
-        // Add Inventory Item
-        html.on('click', '.item-create', this._onItemCreate.bind(this));
+    //     // Add Inventory Item
+    //     html.on('click', '.item-create', this._onItemCreate.bind(this));
 
-        // Delete Inventory Item
-        html.on('click', '.item-delete', (ev) => {
-            const li = $(ev.currentTarget).parents('.item');
-            const item = this.actor.items.get(li.data('itemId'));
-            item.delete();
-            li.slideUp(200, () => this.render(false));
-        });
+    //     // Delete Inventory Item
+    //     html.on('click', '.item-delete', (ev) => {
+    //         const li = $(ev.currentTarget).parents('.item');
+    //         const item = this.actor.items.get(li.data('itemId'));
+    //         item.delete();
+    //         li.slideUp(200, () => this.render(false));
+    //     });
 
-        // Active Effect management
-        html.on('click', '.effect-control', (ev) => {
-            const row = ev.currentTarget.closest('li');
-            const document =
-                  row.dataset.parentId === this.actor.id
-                  ? this.actor
-                  : this.actor.items.get(row.dataset.parentId);
-            onManageActiveEffect(ev, document);
-        });
+    //     // Active Effect management
+    //     html.on('click', '.effect-control', (ev) => {
+    //         const row = ev.currentTarget.closest('li');
+    //         const document =
+    //               row.dataset.parentId === this.actor.id
+    //               ? this.actor
+    //               : this.actor.items.get(row.dataset.parentId);
+    //         onManageActiveEffect(ev, document);
+    //     });
 
-        // Rollable abilities.
-        html.on('click', '.rollable', this._onRoll.bind(this));
+    //     // Rollable abilities.
+    //     html.on('click', '.rollable', this._onRoll.bind(this));
 
-        // Drag events for macros.
-        if (this.actor.isOwner) {
-            let handler = (ev) => this._onDragStart(ev);
-            html.find('li.item').each((i, li) => {
-                if (li.classList.contains('inventory-header')) return;
-                li.setAttribute('draggable', true);
-                li.addEventListener('dragstart', handler, false);
-            });
-        }
+    //     // Drag events for macros.
+    //     if (this.actor.isOwner) {
+    //         let handler = (ev) => this._onDragStart(ev);
+    //         html.find('li.item').each((i, li) => {
+    //             if (li.classList.contains('inventory-header')) return;
+    //             li.setAttribute('draggable', true);
+    //             li.addEventListener('dragstart', handler, false);
+    //         });
+    //     }
 
-        // Clash Dialog
-        html.on('click', '.clashable', this._clashDialog.bind(this));
-    }
+    //     // Clash Dialog
+    //     html.on('click', '.clashable', this._clashDialog.bind(this));
+    // }
 
     /**
      * Create a dialog for clashing with other characters
